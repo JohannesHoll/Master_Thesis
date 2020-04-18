4901f3ec6172920c6267169a7f163f3d6c6e2a2c
Johannes <victor.johannes.holl@gmail.com>
2020-04-18 22:18:32 +0200

    changed saving to file drom "w" to "a"

--- scrapperproject/fiancescraper/fiancescraper/spiders/FinancesNewsScraper.py
+++ scrapperproject/fiancescraper/fiancescraper/spiders/FinancesNewsScraper.py
@@ -17,21 +17,20 @@ class FinanceNewsScraperSpider(scrapy.Spider):
     
     def start_requests(self):
         start_urls = ['https://www.reuters.com/companies/DAIGn.DE/news',
-        #              ]
-        #start_urls = ['https://www.reuters.com/article/us-health-coronavirus-china-autos/chinas-car-market-seen-rebounding-from-virus-as-lockdowns-ease-idUSKCN21R13A',
-                        ]
-         #             'https://www.reuters.com/companies/BMWG.DE/news'        
+                      'https://www.reuters.com/companies/BMWG.DE/news']
+        
         urls = start_urls
+        
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse_newspage)
-            #yield scrapy.Request(url=url, callback=self.parse_article)
+            
             
     def parse_newspage(self, response):
         #links = response.xpath('//a[contains(@href,"/article/")]/text()').extract() #extract article name
         links = response.xpath('//a[contains(@href,"/article/")]/@href').extract() #extract hyperlink
-        print(links)
-        with open('article_links.json', 'w') as fp:
-            json.dump([link for link in links], fp)
+        #print(links)
+        with open('article_links.json', 'a') as fp:
+            json.dump([link + '\n' for link in links], fp)
         for url in links:
             yield response.follow(url = url,callback = self.parse_article)    
         #filepath = 'DC_links1.csv'
@@ -43,21 +42,26 @@ class FinanceNewsScraperSpider(scrapy.Spider):
         #links = response.xpath('//*[contains(@class,"ArticleHeader_headline")]/text()').extract() # extract article headline 
         #links = response.xpath('//*[contains(@class,"ArticleHeader_date")]/text()').extract() # extract article date
         #links = response.xpath('//div[@class="StandardArticleBody_body"]//p/text()').extract()
-        links = response.xpath('//div[@class="StandardArticleBody_body"]//p/text()').extract()
-        #joinedlinks = response.urljoin(links)     
-        #for sel in response.xpath('//div[@class="StandardArticleBody_body"]'):
-        #    item = FiancescraperItem()
-        #    item['articel_text'] = sel.xpath('//p/text()').extract()
-        #    print(item)
-        l = [link for link in links]
-        print(l)
-        print(type(links))
+        #intents = response.xpath('//div[@class="StandardArticleBody_body"]//p/text()').extract()
+        
+        for sel in response.xpath('//div[@class="StandardArticleBody_body"]'):
+            item = FiancescraperItem()
+            item['articel_text'] = sel.xpath('//p/text()').extract()
+            print(item)
+        #intent = []
+        #for i in intents:
+        #    with open('article_intent.json', 'a') as fp:
+        #        json.dump(i + '\n', fp, indent = 6)
+            #intent.append(i)
+        #print(intent)
+        #print(type(links))
         #print(len(links))
         #print(type(l))
-        counter=1
-        with open('article_intent%s.json' % counter, 'w') as fp:
-            json.dump(l, fp, indent = 6)
-            counter += 1
+        #counter=1
+        #with open('article_intent%s.json' % counter, 'w') as fp:
+        #with open('article_intent.json', 'a') as fp:
+        #    json.dump([l + '\n' for l in intent], fp, indent = 6)
+            #counter += 1
         #ch_titles_ext = [t.strip() for t in links]
         # Store this in our dictionary
         #dc_dict[links] = ch_titles_ext
