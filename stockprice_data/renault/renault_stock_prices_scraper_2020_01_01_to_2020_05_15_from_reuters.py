5b2645ac9980c9979a2f8a83983d671abf01cb63
Johannes <victor.johannes.holl@gmail.com>
2020-05-15 23:33:58 +0200

    add code to retrieve stock prices from 01/01/2020 to 15/05/2020

---
+++ stockprice_data/renault/renault_stock_prices_scraper_2020_01_01_to_2020_05_15_from_reuters.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 14 21:49:44 2020
+
+@author: victo
+"""
+# needed libraries
+import eikon as ek
+import os
+
+# Reuters API key
+ek.set_app_key('2c673521ea4c45bc809415aaceceea8a2a1ce080')
+
+
+rics = ['RENA.PA'] 
+fields = ['OPEN','HIGH','LOW','CLOSE','VOLUME'] 
+df = ek.get_timeseries(rics=rics,
+                       fields=fields, 
+                       start_date='2020-01-01T07:00:00', 
+                       end_date='2020-05-15T22:30:00', 
+                       interval='minute') 
+print(df)
+
+df.to_csv(r'C:\Users\victo\Master_Thesis\stockprice_data\renault\stock_prices\renault_prices_2020_01_01_to_2020_05_15.csv')#, 
+          #index=False,
+          #header=True)
