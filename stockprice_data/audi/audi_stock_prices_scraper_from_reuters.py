b32cff04a990a0ac229bf45d56301153f3374b99
Johannes <victor.johannes.holl@gmail.com>
2020-05-16 16:33:02 +0200

    modified code to retrieve stock prices from current date

--- stockprice_data/audi/audi_stock_prices_scraper_from_reuters.py
+++ stockprice_data/audi/audi_stock_prices_scraper_from_reuters.py
@@ -6,16 +6,30 @@ Created on Thu May 14 21:49:44 2020
 """
 # needed libraries
 import eikon as ek
+from datetime import datetime, timedelta
 
+#Reuters API Key
 ek.set_app_key('2c673521ea4c45bc809415aaceceea8a2a1ce080')
 
+#date modfication
+current_date = datetime.today().strftime('%Y-%m-%d')
+current_date_for_modification = datetime.today()
+date_minus_one = current_date_for_modification + timedelta(days=-1)
+date_minus_three = current_date_for_modification + timedelta(days=-3)
+modified_date_minus_one = date_minus_one.strftime("%Y-%m-%d")
+modified_date_minus_three = date_minus_three.strftime("%Y-%m-%d")
+print(current_date)
+print(modified_date_minus_one)
+print(modified_date_minus_three)
 
+#RIC, fields, time to receive stock prices
 rics = ['NSUG.DE'] 
 fields = ['OPEN','HIGH','LOW','CLOSE','VOLUME'] 
 df = ek.get_timeseries(rics=rics,
                        fields=fields, 
-                       start_date='2019-05-14T07:00:00', 
-                       end_date='2020-05-14T17:30:00', 
+                       start_date=str(modified_date_minus_one) + 'T07:00:00',#'2020-01-02T09:00:00', 
+                       end_date=str(modified_date_minus_one) + 'T22:01:00',#'2020-01-02T17:30:00', 
                        interval='minute') 
 print(df)
-
+#safe to csv
+df.to_csv(r'C:\Users\victo\Master_Thesis\stockprice_data\audi\stock_prices\audi_prices_' + str(modified_date_minus_one) + '.csv')
--- stockprice_data/bmw/bmw_stock_prices_scraper_from_reuters.py
+++ stockprice_data/bmw/bmw_stock_prices_scraper_from_reuters.py
@@ -4,4 +4,32 @@ Created on Thu May 14 21:49:44 2020
 
 @author: victo
 """
+# needed libraries
+import eikon as ek
+from datetime import datetime, timedelta
 
+#Reuters API Key
+ek.set_app_key('2c673521ea4c45bc809415aaceceea8a2a1ce080')
+
+#date modfication
+current_date = datetime.today().strftime('%Y-%m-%d')
+current_date_for_modification = datetime.today()
+date_minus_one = current_date_for_modification + timedelta(days=-1)
+date_minus_three = current_date_for_modification + timedelta(days=-3)
+modified_date_minus_one = date_minus_one.strftime("%Y-%m-%d")
+modified_date_minus_three = date_minus_three.strftime("%Y-%m-%d")
+print(current_date)
+print(modified_date_minus_one)
+print(modified_date_minus_three)
+
+#RIC, fields, time to receive stock prices
+rics = ['BMWG.DE'] 
+fields = ['OPEN','HIGH','LOW','CLOSE','VOLUME'] 
+df = ek.get_timeseries(rics=rics,
+                       fields=fields, 
+                       start_date=str(modified_date_minus_one) + 'T09:00:00',#'2020-01-02T09:00:00', 
+                       end_date=str(modified_date_minus_one) + 'T22:01:00',#'2020-01-02T17:30:00', 
+                       interval='minute') 
+print(df)
+#safe to csv
+df.to_csv(r'C:\Users\victo\Master_Thesis\stockprice_data\bmw\stock_prices\bmw_prices_' + str(modified_date_minus_one) + '.csv')
