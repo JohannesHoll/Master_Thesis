372d816064671e0ca053330b2b6caa768d480b15
Johannes <victor.johannes.holl@gmail.com>
2020-05-21 16:26:16 +0200

    modification of date variable and add new stock price files

--- stockprice_data/ferrari/ferrari_stock_prices_scraper_from_reuters.py
+++ stockprice_data/ferrari/ferrari_stock_prices_scraper_from_reuters.py
@@ -14,22 +14,22 @@ ek.set_app_key('2c673521ea4c45bc809415aaceceea8a2a1ce080')
 #date modfication
 current_date = datetime.today().strftime('%Y-%m-%d')
 current_date_for_modification = datetime.today()
-date_minus_one = current_date_for_modification + timedelta(days=-4)
-date_minus_three = current_date_for_modification + timedelta(days=-3)
-modified_date_minus_one = date_minus_one.strftime("%Y-%m-%d")
-modified_date_minus_three = date_minus_three.strftime("%Y-%m-%d")
+date_minus_certain_days = current_date_for_modification + timedelta(days=-2)
+#date_minus_three = current_date_for_modification + timedelta(days=-3)
+modified_date = date_minus_certain_days.strftime("%Y-%m-%d")
+#modified_date_minus_three = date_minus_three.strftime("%Y-%m-%d")
 print(current_date)
-print(modified_date_minus_one)
-print(modified_date_minus_three)
+print(modified_date)
+#print(modified_date_minus_three)
 
 #RIC, fields, time to receive stock prices
 rics = ['RACE.MI'] 
 fields = ['OPEN','HIGH','LOW','CLOSE','VOLUME'] 
 df = ek.get_timeseries(rics=rics,
                        fields=fields, 
-                       start_date=str(modified_date_minus_one) + 'T07:00:00',#'2020-01-02T09:00:00', 
-                       end_date=str(modified_date_minus_one) + 'T22:01:00',#'2020-01-02T17:30:00', 
+                       start_date=str(modified_date) + 'T07:00:00',#'2020-01-02T09:00:00', 
+                       end_date=str(modified_date) + 'T22:01:00',#'2020-01-02T17:30:00', 
                        interval='minute') 
 print(df)
 #safe to csv
-df.to_csv(r'C:\Users\victo\Master_Thesis\stockprice_data\ferrari\stock_prices\ferrari_prices_' + str(modified_date_minus_one) + '.csv')
+df.to_csv(r'C:\Users\victo\Master_Thesis\stockprice_data\ferrari\stock_prices\ferrari_prices_' + str(modified_date) + '.csv')
