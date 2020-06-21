a0a0aac9bff0bdadded7a737867e17cddc193082
Victor-Johannes <victor.johannes.holl@gmail.com>
2020-06-21 15:48:25 +0200

    general update

--- newscatcher/newscatcher_finance.py
+++ newscatcher/newscatcher_finance.py
@@ -8,19 +8,27 @@ Created on Sun May 31 13:12:10 2020
 #### necessary libraries ####
 from newscatcher import Newscatcher, describe_url, urls
 
-nc = Newscatcher(website = 'nytimes.com', topic = 'finance')
+nc = Newscatcher(website = 'reuters.com', topic = 'politics')
 
 results = nc.get_news()
 articles = results['articles']
+print(articles)
 nc.print_headlines()
 
 #headlines = results['articles'][0]['headline']
 print(len(articles))
-title = results['articles'][0]['title']
+#title = results['articles'][0]['title']
 
-summary = results['articles'][0]['summary']
+n = 0
+summ = []
+#for n in articles:  
+summary = articles[0]['summary']
+#summ.append(summary)
+  #  n += 1 
+    
+#published = results['articles'][0]['published']
 
-published = results['articles'][0]['published']
+    #print(summary)
 
 print(summary)
 
