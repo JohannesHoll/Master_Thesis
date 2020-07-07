3339fb950ec53e0f9799d8a60e10d5840e1f6583
Victor-Johannes <victor.johannes.holl@gmail.com>
2020-07-08 00:45:18 +0200

    new newsfiles

--- scraperproject/audi/audi_scraper/spiders/AudiNewsScraper.py
+++ scraperproject/audi/audi_scraper/spiders/AudiNewsScraper.py
@@ -3,6 +3,7 @@
 
 # import of relevant libraries
 import scrapy
+from scrapy.spiders import CrawlSpider
 from scrapy.crawler import CrawlerProcess
 from scrapy import Selector
 from scrapy.linkextractors import LinkExtractor
@@ -12,12 +13,17 @@ from scrapy.loader import ItemLoader
 import csv
 from datetime import datetime
 from scrapy_splash import SplashRequest
+from selenium import webdriver
+from scrapy import FormRequest
 
 #creating spider for Audi
 
 class FinanceNewsScraperSpider(scrapy.Spider):
     name = "audinewsarticles"
-    
+
+    #def __init__(self):
+    #    self.driver = webdriver.Firefox(executable_path=r'D:\victo\Documents\Frankfurt School\Master Thesis\geckodriver.exe')
+
     def start_requests(self):
         start_urls = ['https://www.reuters.com/companies/NSUG.DE/news',
                       ]
@@ -26,54 +32,35 @@ class FinanceNewsScraperSpider(scrapy.Spider):
         
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse_newspage)
-        #for url in urls:
-        #    yield SplashRequest(url=url, 
-        #                        callback=self.parse_newspage#, 
-        #                         #args={'wait': 0.5}
-        #                         )
-                
+
     def parse_newspage(self, response):
         links = response.xpath('//a[contains(@href,"/article/")]/@href').extract() #extract hyperlink
         for url in links:
-            yield response.follow(url = url,callback = self.parse_article)    
+            yield scrapy.Request(url=url,
+                                 meta={'splash':{'args':{'html': 1,
+                                                         'wait': 0.5,
+                                                         'har': 1
+                                                         },
+                                                 'endpoint': 'render.json'
+                                                 }
+                                       },
+                                 callback=self.parse_article)
 
     def parse_article(self, response):
-        #links = response.xpath('//*[contains(@class,"ArticleHeader_headline")]/text()').extract() # extract article headline 
-        #links = response.xpath('//*[contains(@class,"ArticleHeader_date")]/text()').extract() # extract article date
-        #links_contents = response.xpath('//div[@class="StandardArticleBody_body"]//p/text()').extract()
-        #intents = response.xpath('//div[@class="StandardArticleBody_body"]//p/text()').extract()
-            
         item = AudiItem()
         item['article_link'] = response.url
         item['article_headline'] = response.xpath('//*[contains(@class,"ArticleHeader_headline")]/text()').extract()
         item['article_date'] = response.xpath('//*[contains(@class,"ArticleHeader_date")]/text()').extract()
         item['article_text'] = response.xpath('//div[@class="StandardArticleBody_body"]//p/text()').extract()
-        
+
         print(item)
-        
+
         #saving data to file.
         path = 'news/'
         file = 'audinews_' + str(datetime.now().strftime("%Y%m%d-%H%M")) + '.csv'
         file_name = open(path + file, 'a')
 
         fieldnames = ['article_link', 'article_headline','article_date','article_text'] #adding header to file
-        #with open(file, 'w', newline='') as f:
-        #    writer = csv.writer(f, delimiter=',')
-        #    writer.writerow(fieldnames)
-        #    writer
-        #writer = csv.DictWriter(file_name, fieldnames=fieldnames)
+
         writer = csv.writer(file_name, lineterminator='\n')
         writer.writerow([item[key] for key in item.keys()])
-        #writer.writerow(fieldnames)
-        #writer.writeheader()
-        #writer.writerow({'article_link': item['article_link'],
-        #                 'article_headline': item['article_headline'],
-        #                 'article_date': item['article_date'],
-        #                 'article_text': item['article_text']}) #writing data into file.
-        #with open('test.csv', 'a', newline ='') as file:
-        #    writer = csv.writer(file, delimiter=',')
-        #    writer.writerow(head for head in fieldnames)
-        #    with open('test.csv', 'a', newline='') as f:
-        #        writer = csv.writer(f, delimiter=',')
-        #        writer.writerow([item[key] for key in item.keys()])
-        
