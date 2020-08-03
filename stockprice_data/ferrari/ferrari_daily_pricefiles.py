0631cfbfc7d3d919d48f29756d8704012717100c
Victor-Johannes <victor.johannes.holl@gmail.com>
2020-08-03 23:29:04 +0200

    update of repository

---
+++ stockprice_data/ferrari/ferrari_daily_pricefiles.py
@@ -0,0 +1,33 @@
+# needed libraries
+import eikon as ek
+from datetime import datetime, timedelta, timezone
+from dateutil.rrule import rrule, DAILY
+import pandas as pd
+import pytz
+import logging
+
+#surpress error
+logger = logging.getLogger('pyeikon')
+logger.setLevel(logging.CRITICAL)
+
+#Reuters API Key
+ek.set_app_key('7562ce3840dd4ebab1a05f901ca0777c959e70e8')
+
+# date modfication
+start_date = datetime(2019, 1, 1, tzinfo=timezone.utc)
+end_date = datetime(2020, 8, 3, tzinfo=timezone.utc)
+date_range = pd.date_range(start=start_date, end=end_date, freq='D')
+# RIC, fields, time to receive stock prices
+rics = ['RACE.MI']
+fields = ['OPEN', 'HIGH', 'LOW', 'CLOSE', 'VOLUME']
+
+sdate = str(start_date)[0:10] + 'T07:00:00'
+edate = str(end_date)[0:10] + 'T22:01:00'
+df = ek.get_timeseries(rics=rics,
+                       fields=fields,
+                       start_date=sdate,
+                       end_date=edate,
+                       interval='daily')
+
+# safe to csv
+df.to_csv(r'C:\Users\victo\Master_Thesis\stockprice_data\ferrari\daily_stock_prices\ferrari_daily_prices.csv')
