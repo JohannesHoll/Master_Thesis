b0f9bbd47a7b74e3d17d650c86373b4ec4578dc8
Victor-Johannes <victor.johannes.holl@gmail.com>
2020-08-08 22:54:00 +0200

    new structure of folder

---
+++ correlation/audi/daily/correlation_audi.py
@@ -0,0 +1,66 @@
+###necessary libaries###
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import glob
+import os
+from datetime import datetime
+import matplotlib.pyplot as plt
+import re
+
+# file where csv files lies
+path = r'C:\Users\victo\Master_Thesis\merging_data\audi\minutely\merged_files'
+#path = r'C:\Users\victo\Master_Thesis\semanticanalysis\analysis_with_flair\audi\outcome_using_flair'
+all_files = glob.glob(os.path.join(path, "*.csv"))
+
+# read files to pandas frame
+list_of_files = []
+
+for filename in all_files:
+    list_of_files.append(pd.read_csv(filename,
+                                     sep=',',
+                                     )
+                         )
+
+# Concatenate all content of files into one DataFrames
+concatenate_dataframe = pd.concat(list_of_files,
+                                      ignore_index=True,
+                                      axis=0,
+                                      )
+
+print(concatenate_dataframe)
+
+new_df = concatenate_dataframe[['return_one_hot_encoded',
+                                'flair_sentiment_header',
+                                'flair_sentiment_content',
+                                'compound_vader_header',
+                                'compound_vader_articel_content',
+                                'polarity_textblob_sentiment_header',
+                                'polarity_textblob_sentiment_content']]
+
+new_df = new_df.replace(0, np.nan)
+
+print(sum(new_df['flair_sentiment_content']))
+
+#print(new_df)
+
+# header = []
+# for i in new_df['flair_sentiment_header']:
+#     i = str(i)
+#new_df['flair_sentiment_header'] = new_df['flair_sentiment_header'].astype('string')
+# for i in new_df['flair_sentiment_header']:
+#     #i = str(i)
+#     if r'POSITIVE' in i.values:
+#         #value_postive = i.str.extract(r'\((.*?)\)', expand=False)
+#         print(i)
+#     # if new_df[new_df.flair_sentiment_header.str.contains('POSTIVE', case=False)]:
+    #     value_postive = new_df['flair_sentiment_header'].str.extract(r'\((.*?)\)', expand=False)
+    #     print(value_postive)
+    # elif new_df[new_df.flair_sentiment_header.str.contains('NEGATIVE', case=False)]:
+    #     value_negative = new_df['flair_sentiment_header'].str.extract(r'\((.*?)\)', expand=False)
+    #     print(value_negative)
+
+# print(new_df)
+#
+# corr = new_df.corr()
+# corr = corr.fillna(0)
---
+++ correlation/volvo/minutely/correlation_volvo.py
@@ -0,0 +1,54 @@
+###necessary libaries###
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import glob
+import os
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+# file where csv files lies
+path = r'C:\Users\victo\Master_Thesis\merging_data\volvo\merged_files'
+all_files = glob.glob(os.path.join(path, "*.csv"))
+
+# read files to pandas frame
+list_of_files = []
+
+for filename in all_files:
+    list_of_files.append(pd.read_csv(filename,
+                                     sep=',',
+                                     )
+                         )
+
+# Concatenate all content of files into one DataFrames
+concatenate_dataframe = pd.concat(list_of_files,
+                                      ignore_index=True,
+                                      axis=0,
+                                      )
+
+print(concatenate_dataframe)
+
+new_df = concatenate_dataframe[['return_one_hot_encoded',
+                                'flair_sentiment_header_score',
+                                'flair_sentiment_content_score',
+                                'compound_vader_header',
+                                'compound_vader_articel_content',
+                                'polarity_textblob_sentiment_header',
+                                'polarity_textblob_sentiment_content']]
+
+new_df[['return_one_hot_encoded',
+        'flair_sentiment_header_score',
+        'flair_sentiment_content_score',
+        'compound_vader_header',
+        'compound_vader_articel_content',
+        'polarity_textblob_sentiment_header',
+        'polarity_textblob_sentiment_content']] = new_df[['return_one_hot_encoded',
+                                                          'flair_sentiment_header_score',
+                                                          'flair_sentiment_content_score',
+                                                          'compound_vader_header',
+                                                          'compound_vader_articel_content',
+                                                          'polarity_textblob_sentiment_header',
+                                                          'polarity_textblob_sentiment_content']].fillna(0)
+
+corr = new_df.corr()
+print(corr)
